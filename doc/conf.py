d442aa5f1605549e325463868e208870861976bc
ahuang11 <ahuang11@illinois.edu>
2021-04-01 22:09:12 -0500

    Update

--- doc/conf.py
+++ doc/conf.py
@@ -14,15 +14,19 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import datetime
+
+import ahlive
+
 
 # -- Project information -----------------------------------------------------
-
-project = 'pyrambles'
-copyright = '2021, Andrew Huang'
-author = 'Andrew Huang'
+year = datetime.datetime.utcnow().year
+project = "pYdeas"
+copyright = f"2021 to {year}, Andrew Huang"
+author = "Andrew Huang"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = ahlive.__version__
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +34,15 @@ release = '0.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ["nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +50,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+# allow errors
+nbsphinx_allow_errors = True
+
+# Use this kernel instead of the one stored in the notebook metadata:
+nbsphinx_kernel_name = "python3"
