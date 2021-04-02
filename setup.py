3edd50fb645d0f2313d9e32c6b912f1cc4f5cfaf
ahuang11 <ahuang11@illinois.edu>
2021-04-01 22:21:23 -0500

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pYdeas",
+    description="Python ideas",
+    url="https://github.com/ahuang11/pydeas",
+    packages=["pydeas"],
+    include_package_data=True,
+    install_requires=[],
+    keywords=["python", "ideas"],
+    zip_safe=True,
+)
