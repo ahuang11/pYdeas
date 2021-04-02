3b41987b89a39951855d47d67392382be47a6f5e
ahuang11 <ahuang11@illinois.edu>
2021-04-01 22:11:04 -0500

    Fix import

--- doc/conf.py
+++ doc/conf.py
@@ -16,8 +16,6 @@
 
 import datetime
 
-import ahlive
-
 
 # -- Project information -----------------------------------------------------
 year = datetime.datetime.utcnow().year
@@ -26,7 +24,7 @@ copyright = f"2021 to {year}, Andrew Huang"
 author = "Andrew Huang"
 
 # The full version, including alpha/beta/rc tags
-release = ahlive.__version__
+release = 0.0.0
 
 
 # -- General configuration ---------------------------------------------------
